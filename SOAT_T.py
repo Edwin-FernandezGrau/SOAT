ad2de0cbfb216fc258070ea73f428e68f8320104
Edwin Leonardo <edwin.fernandezgrau@gmail.com>
2021-02-24 23:39:48 -0500

    creacion

--- SOAT_T.py
+++ SOAT_T.py
@@ -8,43 +8,64 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-############ titulo ###########
 
 
+### condiciones de la pagina e importación del logo
 st.set_page_config(page_title= "SOAT Crecer Seguros",layout="wide")
-
 image = Image.open('logo.PNG')
-
 st.sidebar.image(image, width = 300)
 
 
-
-
+###### importamos información de los excel
 ruta = 'Tarifario SOAT 202102.xlsx'
-t_digital = pd.read_excel(ruta,sheet_name= "TD_Crecer_M",header = 7, index_col =[0,1],engine ='openpyxl' )
-t_fisico = pd.read_excel(ruta,sheet_name= "TF_Crecer_M",header = 7, index_col =[0,1] ,engine ='openpyxl')
+t_digital = pd.read_excel(ruta,sheet_name= "TD_Crecer_M",header = 7, index_col =[0,1,2],engine ='openpyxl' )
+t_fisico = pd.read_excel(ruta,sheet_name= "TF_Crecer_M",header = 7, index_col =[0,1,2] ,engine ='openpyxl')
 
-#,engine ='openpyxl'
+t_digital["llave"] = t_digital.index.copy()
+t_digital.set_index("llave",append= True , inplace = True)
+t_fisico["llave"] = t_fisico.index.copy()
+t_fisico.set_index("llave",append= True , inplace = True)
 
-uso = list(t_digital.index.unique(0))
 
 
+###### asignamos las comisiones
+b_comision = pd.DataFrame(t_digital["CATEGORIA"].copy())
+b_comision["com default"] = 0
 
-departamento = list(t_digital.columns)
-departamento = departamento[2:]
+b_comision.loc["CARGA","com default"] =14
+b_comision.loc["PARTICULAR","com default"] = 14
+b_comision.loc["TRANSPORTE URBANO","com default"] = 10
+
+usos_comision = (list(b_comision.index.unique(0)))
+b_comision.loc[(usos_comision ,['Vehículo Menor']),"com default"] = 8 # sin importar el uso, la com de VM es 8%
+
+
+### partimos asumiendo que la comisión calculada es la misma que default
+b_comision["com calculada"] = b_comision["com default"].copy()
+
+#comisiones posibles a calcular ( los que se mostraran para seleccionar)
+
+com_pa = list(range(7,21))   #particular
+com_tu = list(range(5,16))   #transporte urbano
+com_ca = list(range(7,21))   # carga
+com_vm = list(range(5,16))   # vehículo menor
+
+
+#valores de uso y departamento a seleccionar
+uso = list(t_digital.index.unique(0))     #La lista unica de usos
+departamento = list(t_digital.columns)   #lista de columnas
+departamento = departamento[1:]          # excluimos las dos primeeras (categoria y descripción)
+
 
 st.title(""" Tarifario SOAT """)
-
- 
 st.sidebar.header('Parametros')
+
+
 
 
 def main():
 
-   
-  
-   
-    fi_uso = st.sidebar.multiselect("Seleccione el USO", uso,uso)
+    fi_uso = st.multiselect("Seleccione el USO", uso,uso)
     
     if not  fi_uso:
         st.error("Seleccione al menos un USO")
@@ -52,30 +73,51 @@ def main():
     else:
         clase =  list(t_digital.loc[fi_uso].index.unique(1))
         
-        fi_clase = st.sidebar.multiselect("Seleccione la CLASE",clase ,clase)
+        fi_clase = st.multiselect("Seleccione la CLASE",clase ,clase)
         
-        fi_departamento = st.sidebar.multiselect("Seleccione la DEPARTAMENTO", departamento, departamento[:6])
+        fi_departamento = st.multiselect("Seleccione la DEPARTAMENTO", departamento, departamento[:12])                
         
-    
-    
         
-        fi_departamento.insert(0,"DESCRIPCION")
+        
+        
+        # asignamos las posibles comisiones
+        fi_com_pa = st.sidebar.selectbox("Seleccione comisión PARTICULAR", com_pa, index= 7 )
+        fi_com_tu = st.sidebar.selectbox("Seleccione comisión TRANSPORTE URBANO", com_tu, index= 5 )
+        fi_com_ca = st.sidebar.selectbox("Seleccione comisión CARGA", com_ca, index= 7 )
+        fi_com_vm = st.sidebar.selectbox("Seleccione comisión VEHÍCULO MENOR", com_vm, index= 3 )
+        
+        
+        # introducimos en la base de comisiones
+        b_comision.loc["CARGA","com calculada"] = fi_com_ca
+        b_comision.loc["PARTICULAR","com calculada"] = fi_com_pa
+        b_comision.loc["TRANSPORTE URBANO","com calculada"] =  fi_com_tu
+        b_comision.loc[(usos_comision ,['Vehículo Menor']),"com calculada"] = fi_com_vm
+        
+        
+        #calculamos el efecto x comisiones
+        b_comision["efecto"] = (100-b_comision["com default"])/(100-b_comision["com calculada"])
+        
+        #b_comision["efecto"] = 1.5
+        t_digital1 = t_digital[departamento].mul(b_comision["efecto"], axis = 0 , level = "llave").round()
+        t_fisico1 = t_fisico[departamento].mul(b_comision["efecto"], axis = 0 , level = "llave").round()
+        
         
         st.subheader('DIGITAL')
-        #st.write(""" DIGITAL """)  
-        
-        bd = t_digital[t_digital.index.isin(fi_uso, level=0)]
+      
+        bd =  t_digital1.reset_index(level = "llave" , drop = True) 
+        bd = bd[bd.index.isin(fi_uso, level=0)]
         bd = bd[bd.index.isin(fi_clase, level=1)]
         
-        st.dataframe(bd[fi_departamento], width=1200, height=600)
+        st.dataframe(bd[fi_departamento], width=1600, height=600)
      
             
         st.subheader('FISICO') 
         
-        bf = t_fisico[t_fisico.index.isin(fi_uso, level=0)]
+        bf =  t_fisico1.reset_index(level = "llave" , drop = True) 
+        bf = bf[bf.index.isin(fi_uso, level=0)]
         bf = bf[bf.index.isin(fi_clase, level=1)]
         
-        st.dataframe( bf[fi_departamento], width=1200, height=600)
+        st.dataframe( bf[fi_departamento], width=1600, height=600)
     
         st.markdown('---')
         
